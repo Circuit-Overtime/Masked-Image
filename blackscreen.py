5c31baa065ba8dc082cd3bf865576501b44d432d
Circuit-Overtime <74301576+Circuit-Overtime@users.noreply.github.com>
2023-05-22 20:54:41 +0530

    Update blackscreen.py

--- blackscreen.py
+++ blackscreen.py
@@ -1,39 +1,47 @@
 import cv2
 import numpy as np
 
-
-def create_color_mask(image, color_lower, color_upper):
-    
+# Function to create a red color mask
+def create_red_mask(image):
+    # Convert the image to the HSV color space
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    mask = cv2.inRange(hsv, color_lower, color_upper)
+    # Define the lower and upper bounds for red color
+    lower_red = np.array([0, 50, 50])
+    upper_red = np.array([10, 255, 255])
+    upper_red2 = np.array([170, 255, 255])
+
+    # Create a mask using the red color range
+    mask1 = cv2.inRange(hsv, lower_red, upper_red)
+    mask2 = cv2.inRange(hsv, lower_red, upper_red2)
+    mask = cv2.bitwise_or(mask1, mask2)
 
     return mask
 
-
-def apply_color_mask(image, mask):
+# Function to apply the red color mask on the image
+def apply_red_mask(image, mask):
     
-    inverse_mask = cv2.bitwise_not(mask)
+    foreground = cv2.bitwise_and(image, image, mask=mask)
 
 
-    background = cv2.bitwise_and(image, image, mask=inverse_mask)
+    background = np.zeros_like(image)     # Create a black background
 
-    return background
+    return foreground, background
 
+# Load the image
+image = cv2.imread('test.png')
 
-image = cv2.imread('test.jpg')
+# Create the red color mask
+red_mask = create_red_mask(image)
 
-# Define the color range for masking (in HSV)
-color_lower = np.array([0, 100, 100])  # Lower range of color (in HSV)
-color_upper = np.array([10, 255, 255])  # Upper range of color (in HSV)
+# Apply the red color mask on the image
+foreground, background = apply_red_mask(image, red_mask)
 
+# Show the original image, foreground (masked image), and black background
 
-mask = create_color_mask(image, color_lower, color_upper)
-
-
-masked_image = apply_color_mask(image, mask)
-
-# Show the original image and the masked image
-cv2.imshow('Masked Image', masked_image)
+cv2.imshow('Foreground (Masked Image)', foreground)
+cv2.imshow('Background', background)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+#BETTER USE PHOTOSHOP :)
